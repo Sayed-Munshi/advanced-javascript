57b93de7ea78e6fcfcef15e7b33551b30eca4a46
Sayed Munshi <mdsayed3000@gmail.com>
2020-08-31 15:41:03 +0600

    double vs triple equal

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,26 @@
+// double equal
+
+const first = 2;
+const second = 2
+const second = "2"
+
+if(first == second){
+    console.log("condition is true");
+}
+
+else{
+    console.log("condition is False");
+}
+
+// triple equal
+
+const first = 2;
+const second = "2"
+
+if(first === second){
+    console.log("condition is true");
+}
+
+else{
+    console.log("condition is False");
+}
