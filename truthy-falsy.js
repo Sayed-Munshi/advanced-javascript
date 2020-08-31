47d9cfe6562a84816f65f85a428395563fbebdb1
Sayed Munshi <mdsayed3000@gmail.com>
2020-08-31 13:59:06 +0600

    True and False Condition

---
+++ truthy-falsy.js
@@ -0,0 +1,21 @@
+// 0, "", undefined, Null, NaN, false (False condition)
+//  '0', " ", "uhdiuh", (True condition)
+
+
+// const age = 5;
+// if(age > 0){
+//     console.log("Condition is True");
+// }
+// else{
+//     console.log("condition is False");
+// }
+
+// const name = "0";
+let name = NaN;
+console.log(name);
+if(name){
+    console.log("Condition is True");
+}
+else{
+    console.log("condition is False");
+}
