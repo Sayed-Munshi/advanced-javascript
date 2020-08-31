958273c7864eaa3a6cb70d8c5ad56e2ecddd274d
Sayed Munshi <mdsayed3000@gmail.com>
2020-08-31 15:27:49 +0600

    Null vs Undefined clear for interview

---
+++ undefined-vs-null.js
@@ -0,0 +1,26 @@
+// undefind
+
+let pakhi;
+
+function add(num1, num2){
+    console.log(num1 + num2);
+    return;
+}
+
+function add(num1, num2){
+    console.log(num2);
+    
+}
+const result = add(13);
+console.log(result);
+
+const values = {name:"hello", phone:868442}
+console.log(values.gf);
+
+let fun = undefined;
+console.log(fun);
+
+
+// Null 
+
+
